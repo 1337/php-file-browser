f3db643cee930e0f4187022a88073259966a55f3
Brian <b2lai@uwaterloo.ca>
2012-04-15 23:20:25 -0400

    -

--- browser.php
+++ browser.php
@@ -32,7 +32,7 @@
     define ('DOWNLOAD_HERE'     , 6);
     define ('UPLOAD_HERE'       , 7);
     define ('LOGIN'             , 8);
-    define ('AJAX_FILE_TRANSFER', 9);
+    define ('AJAX_FILE_SAVE'    , 9);
     define ('JSON_TREE'         , 10);
     define ('DEBUG'             , 99);
 
@@ -247,92 +247,23 @@
 
     // For modes with html heads, print head now.
     if (in_array ($mode, array (FRAME, TREE, EDITOR, DOWNLOAD_HERE, LOGIN))) {
+        $css_files = array (
+            'http://fonts.googleapis.com/css?family=Open+Sans',
+            'http://fonts.googleapis.com/css?family=Droid+Sans+Mono',
+            FILE_ROOT . 'scripts/codemirror/lib/codemirror.css',
+            FILE_ROOT . 'scripts/codemirror/theme/monokai.css',
+            FILE_ROOT . 'css/custom.css'
+        );
 ?>
         <html><head>
-            <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Open+Sans' />
-            <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Droid+Sans+Mono' />
-            <link rel='stylesheet' type='text/css' href='<?php echo FILE_ROOT; ?>scripts/codemirror/lib/codemirror.css' />
-            <link rel='stylesheet' type='text/css' href='<?php echo FILE_ROOT; ?>scripts/codemirror/theme/monokai.css' />
-            <link rel='stylesheet' type='text/css' href='<?php echo FILE_ROOT; ?>css/custom.css' />
+            <?php foreach ($css_files as $css) { echo "<link rel='stylesheet' type='text/css' href='$css' />"; } ?>
             <script type="text/javascript" src='https://raw.github.com/1337/Lazyload/master/lazyload.min.js'></script>
+            <script type="text/javascript" src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js'></script>
+            
             <script type='text/javascript'>
                 var populate_tree = null;
                 var populate_tree_ex = null;
                 var parent_path = null;
-
-                $L (['http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js'], function () {
-                    $(document).ready (function () {
-                        var rot13 = function (s) {
-                            return s.replace(/[a-zA-Z]/g,function(c){
-                            return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);})
-                        };
-
-                        // global
-                        parent_path = function (path) {
-                            return path.substr(0, path.lastIndexOf('/'));
-                        }
-
-                        // global
-                        populate_tree_ex = function (id, path) {
-                            $.getJSON ('<?php echo basename (__FILE__); ?>', {
-                                    'mode': '<?php echo JSON_TREE; ?>',
-                                    'cwd': path,
-                                    'param1': <?php echo DIRS_AND_FILES; ?>
-                                }, function (data) {
-                                    populate_tree (id, data);
-                                }
-                            );
-                            $('#filetree_head').html ('<a href="#" onclick="javascript:populate_tree_ex(\'' + id + '\', \'' + parent_path (path) + '\');">' +
-                                "<img src='http://img707.imageshack.us/img707/1033/iconfolderup.gif' /" + ">" +
-                                "</a><b>" + path + "</b>"
-                            );
-                        };
-
-                        // global
-                        populate_tree = function (id, data) {
-                            var ctl = $('#' + id);
-                            var i = 0;
-                            var path_style = 'border-left: 3px #3399EE solid; font-weight: bold;';
-                            var file_style = '';
-
-                            ctl.html (""); // clear it
-                            for (x in data) {
-                                if (data[x].type == 'dir') { // folder
-                                    var fi = data[x].perm;
-                                    var isdir = true;
-                                    var link = '<a href="#" onclick="javascript:populate_tree_ex(\'' + id + '\', \'' + data[x].path + '/' + data[x].name + '\');">' +
-                                                   data[x].name +
-                                               '</a>';
-                                } else { // file
-                                    var fi = '<a href="?file=' + data[x].name + '&amp;mode=3">' + data[x].size + '</a>';
-                                    var isdir = false;
-                                    var link = '<a href="?cwd=' + data[x].path +
-                                                 '&amp;file=' + data[x].name +
-                                                 '&amp;mode=2" ' +
-                                                 'target="editor">' + data[x].name + '</a>';
-                                }
-                                $('<tr />').append(
-                                    '<td style="' + (isdir ? path_style : file_style) + '">' +
-                                        '<input type="checkbox" name="c' + i + '" value="1" /' + '>' +
-                                        '<input type="hidden" name="f' + i + '" ' +
-                                               'value="' + data[x].path + '/' + data[x].name +
-                                    '" /></td>' +
-                                    '<td>' +
-                                        link +
-                                        '<span class="small rfloat">' +
-                                            fi +
-                                        '</span>' +
-                                    '</td>')
-                                .appendTo(ctl);
-                            }
-                        };
-
-
-                        if ($('#filetree').length >= 1) {
-                            populate_tree_ex ('filetree', '<?php echo $cwd; ?>');
-                        }
-                    });
-                });
             </script>
         </head>
 <?php
@@ -360,14 +291,15 @@
                 </label>
                 <br />
                 <label>
-                    <input type='radio' name='act' value='archive'>
-                    Archive
-                </label><br /><br />
+                    <input type='radio' name='act' value='archive'> Archive
+                </label>
+                <br />
+                <br />
                 <input type='hidden' name='cwd' value='<?php echo $cwd; ?>' />
-                <input type='hidden' name='mode' value='5' />
+                <input type='hidden' name='mode' value='<?php echo GROUP_ACTIONS; ?>' />
                 <input type='submit' />
             </form>
-            <form method='post' target='tree' action='?mode=7'
+            <form method='post' target='tree' action='?mode=<?php echo UPLOAD_HERE; ?>'
                   enctype='multipart/form-data'>
             <!-- ?mode=7 is needed -->
                 <p class='header'>Upload</p>
@@ -412,44 +344,102 @@
                     </tr>
                     <tr><td></td>
                         <td>
-                            <input type='hidden' name='mode' value='4' />
+                            <input type='hidden' name='mode' value='<?php echo COMMAND_LINE; ?>' />
                             <input type='hidden' name='cwd' value='<?php echo $cwd; ?>' />
                             <input type='submit' />
                         </td>
                     </tr>
                 </table>
             </form>
-            <p>Commands: chmod(param1,param2), cp(param1,param2), delete(param1), exec(param1),
-                mkdir(param1), mkfile(param1), mv(param1,param2),
-                rename(param1,param2), rmdir(param1), touch(param1)</p>
+            <p>Commands: chmod(param1,param2), cp(param1,param2),
+                mkdir(param1), mv(param1,param2),
+                rmdir(param1), touch(param1)</p>
+            <script type="text/javascript">
+                $(document).ready (function () {
+                    var rot13 = function (s) {
+                        return s.replace(
+                            /[a-zA-Z]/g,
+                            function (c) {
+                                return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);
+                            }
+                        );
+                    };
+
+                    // global scope
+                    parent_path = function (path) {
+                        return path.substr(0, path.lastIndexOf('/'));
+                    }
+
+                    // global scope
+                    populate_tree_ex = function (id, path) {
+                        $.getJSON ('<?php echo basename (__FILE__); ?>', {
+                                'mode': '<?php echo JSON_TREE; ?>',
+                                'cwd': path,
+                                'param1': <?php echo DIRS_AND_FILES; ?>
+                            }, function (data) {
+                                populate_tree (id, data);
+                            }
+                        );
+                        $('#filetree_head').html ('<a href="#" onclick="javascript:populate_tree_ex(\'' + id + '\', \'' + parent_path (path) + '\');">' +
+                            "<img src='http://img707.imageshack.us/img707/1033/iconfolderup.gif' /" + ">" +
+                            "</a><b>" + path + "</b>"
+                        );
+                    };
+
+                    // global scope
+                    populate_tree = function (id, data) {
+                        var ctl = $('#' + id);
+                        var i = 0;
+
+                        ctl.html (""); // clear it
+                        for (x in data) {
+                            if (data[x].type == 'dir') { // folder
+                                var fi = data[x].perm;
+                                var isdir = true;
+                                var link = $('<a />', {
+                                    'href': '#',
+                                    'click': function () {
+                                        populate_tree_ex(id, data[x].path + '/' + data[x].name);
+                                    },
+                                    'text': data[x].name
+                                });
+                            } else { // file
+                                var fi = '<a href="?file=' + data[x].name + '&amp;mode=3">' + data[x].size + '</a>';
+                                var isdir = false;
+                                var link = $('<a />', {
+                                    'target': 'editor',
+                                    'href': '?cwd=' + data[x].path + 
+                                            '&file=' + data[x].name +
+                                            '&mode=<?php echo EDITOR; ?>',
+                                    'text': data[x].name
+                                });
+                            }
+                            var td1 = $('<td class="' + (isdir ? 'path_style' : 'file_style') + '">' +
+                                            '<input type="checkbox" name="c' + i + '" value="1" />' +
+                                            '<input type="hidden" name="f' + i + '" ' +
+                                                   'value="' + data[x].path + '/' + data[x].name +
+                                        '" /></td>');
+                            var td2 = $('<td />');
+                            td2.append(link)
+                               .append('<span class="small rfloat">' +
+                                           fi +
+                                       '</span>');
+                            $('<tr />')
+                                .append(td1)
+                                .append(td2)
+                                .appendTo(ctl);
+                        }
+                    };
+
+                    populate_tree_ex ('filetree', '<?php echo $cwd; ?>');
+                });
+            </script>
 <?php
     break; case EDITOR:
         // if I need to open/save a file then show...
         if (!strlen ($file) || !is_file ($file)) {
             die ("To begin, click on a file name in the file panel.");
         }
-
-        if (vars ('content')) { // save?
-            $content = vars ('content');
-
-            //pretend this is a backup
-            if ($config['BACKUP_BEFORE_SAVING'] === true) {
-                if (!file_exists ($file->backup_file ())) { // copy only if not exists (saves first file of date)
-                    copy ($file, $file->backup_file ());
-                }
-                chmod ( // inherit file permissions
-                    $file->backup_file (),
-                    fileperms (__FILE__)
-                );
-            }
-
-            if (file_put_contents ($file, $content) === false) {
-                echo("<p><b>$file</b> was <span style='color:red'>NOT</b> saved.</p>");
-            } else {
-                echo("<p><b>$file</b> is supposedly saved.</p>");
-            }
-        }
-
         $content = file_get_contents ($file);
 ?>
         <body style='overflow: hidden;'>
@@ -470,21 +460,19 @@
                      '<?php echo FILE_ROOT; ?>scripts/codemirror/mode/clike/clike.js',
                      '<?php echo FILE_ROOT; ?>scripts/codemirror/mode/php/php.js'], function () {
                     $(document).ready(function () {
-                        if ($('#content').length >= 1) {
-                            var editor = CodeMirror.fromTextArea(document.getElementById("content"), {
-                                lineNumbers: true,
-                                theme: "monokai",
-                                mode: "application/x-httpd-php",
-                                indentUnit: 4,
-                                smartIndent: true,
-                                tabSize: 4,
-                                indentWithTabs: false,
-                                matchBrackets: true,
-                                pollInterval: 200,
-                                undoDepth: 999,
-                                value: $('#content').val()
-                            });
-                        }
+                        var editor = CodeMirror.fromTextArea(document.getElementById("content"), {
+                            lineNumbers: true,
+                            theme: "monokai",
+                            mode: "application/x-httpd-php",
+                            indentUnit: 4,
+                            smartIndent: true,
+                            tabSize: 4,
+                            indentWithTabs: false,
+                            matchBrackets: true,
+                            pollInterval: 200,
+                            undoDepth: 999,
+                            value: $('#content').val()
+                        });
                     });
                 });
             </script>
@@ -502,15 +490,14 @@
         $param2 = htmlspecialchars (urldecode ($param2));
 
         switch (strtolower ($act)) {
-            case 'mv'; case 'rename':    rename ($param1, $param2); break;
-            case 'chmod':                chmod ($param1, $param2);  break;
-            case 'cp':                   copy ($param1, $param2);   break;
-            case 'exec':                 exec ($param1);            break;
-            case 'mkdir':                mkdir ($param1);           break;
-            case 'mkfile'; case 'touch': touch ($param1);           break;
-            case 'delete'; case 'rm':    unlink ($param1);          break;
-            case 'rmdir':                rmdir ($param1);           break;
-            default:                     die("No such command: $act");
+            case 'mv':    rename ($param1, $param2); break;
+            case 'chmod': chmod ($param1, $param2);  break;
+            case 'cp':    copy ($param1, $param2);   break;
+            case 'mkdir': mkdir ($param1);           break;
+            case 'touch': touch ($param1);           break;
+            case 'rm':    unlink ($param1);          break;
+            case 'rmdir': rmdir ($param1);           break;
+            default:      die("No such command: $act");
         }
 
         $cf = basename ($_SERVER['SCRIPT_FILENAME']);
@@ -526,7 +513,7 @@
 
             if ($param1 === '1') { // checkbox for this file is enabled
                 switch (strtolower ($a)) {
-                    case 'delete'; case 'rm'; case 'rmdir':
+                    case 'rm'; case 'rmdir':
                         unlink ($param2);
                         break;
                     case 'archive':
@@ -613,7 +600,7 @@
                     </form>
                 </div>
 <?php
-    break; case AJAX_FILE_TRANSFER:
+    break; case AJAX_FILE_SAVE:
         // if I need to open/save a file then show...
         if (!strlen ($file) || !is_file ($file)) {
             if (vars ('content')) { // save?
